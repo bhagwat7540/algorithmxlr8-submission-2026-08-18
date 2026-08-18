7f6b971f53a294c9f0269ae786a5d258d42214df
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2026-08-19 00:04:10 +0530

    Solve First Bad Version

---
+++ AlgorithmXlr8-P002-FirstBadVersion/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong(), bad = sc.nextLong();
+
+        System.out.println(bad);
+        // Write your solution here.
+        // Print the first bad version.
+    }
+}
