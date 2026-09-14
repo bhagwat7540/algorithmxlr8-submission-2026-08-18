7851a472ea58ad8d176676738350d90385da64b3
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2026-09-14 21:33:56 +0530

    Solve Number of Provinces

---
+++ AlgorithmXlr8-P017-NumberOfProvinces/Solution.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your solution here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0 ; i<n ; i++) adj.add(new ArrayList<>());
+
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                int x = sc.nextInt();
+                if(i != j && x == 1) {
+                    adj.get(i).add(j);
+                }
+            }
+        }
+
+        boolean[] vis = new boolean[n];
+        int cnt = 0;
+        for(int i=0 ; i<n ; i++) {
+            if(!vis[i]) {
+                //dfs(i, vis, adj)
+                bfs(i, vis, adj);
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static void bfs(int node, boolean[] vis, List<List<Integer>> adj) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(node);
+        vis[node] = true;
+
+        while(!queue.isEmpty()) {
+            int currNode = queue.poll();
+            for(int adjNode : adj.get(currNode)) {
+                if(!vis[adjNode]) {
+                    queue.offer(adjNode);
+                    vis[adjNode] = true;
+                }
+            }
+        }
+    }
+}
