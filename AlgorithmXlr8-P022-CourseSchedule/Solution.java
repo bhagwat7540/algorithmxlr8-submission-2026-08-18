5401ba7ee5a808a5fc75c8ee3c3d2d8a9d12cfe2
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2026-09-16 02:32:36 +0530

    Solve Course Schedule

---
+++ AlgorithmXlr8-P022-CourseSchedule/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Write your solution here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<>(); 
+        for(int i=0 ; i<n ; i++) adj.add(new ArrayList<>());
+
+        int[] indegree = new int[n];
+        for(int i=0 ; i<m ; i++) {
+            int u = sc.nextInt(), v = sc.nextInt();
+            adj.get(v).add(u);
+            indegree[u]++;
+        }
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        for(int i=0 ; i<n ; i++) {
+            if(indegree[i] == 0) queue.offer(i);
+        }
+
+        List<Integer> topo = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            topo.add(node);
+
+            for(int adjNode : adj.get(node)) {
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0) queue.offer(adjNode);
+            }
+        }
+
+        if(topo.size() != n) System.out.println(false);
+        else System.out.println(true);
+    }
+}
